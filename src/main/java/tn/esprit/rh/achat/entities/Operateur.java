021d58a0e755b99123609a5bbc189443837664ea
saiefsmairi <saieftaher@gmail.com>
2022-11-07 22:01:51 +0100

    add tests

--- src/main/java/tn/esprit/rh/achat/entities/Operateur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -34,5 +34,22 @@ public class Operateur implements Serializable{
 	@OneToMany
 	@JsonIgnore
 	private Set<Facture> factures;
-	
+
+	public Operateur(Long idOperateur, String nom, String prenom, String password) {
+		this.idOperateur = idOperateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Operateur{" +
+				"idOperateur=" + idOperateur +
+				", nom='" + nom + '\'' +
+				", prenom='" + prenom + '\'' +
+				", password='" + password + '\'' +
+				", factures=" + factures +
+				'}';
+	}
 }
